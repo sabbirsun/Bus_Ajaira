33fd2ec8773985b1f6ed76d40e8744c6832db08b
Bm Shadman Sakib Mahee <fareless.ranger@gmail.com>
2023-04-06 17:26:03 +0600

    hudai!

---
+++ lol.js
@@ -0,0 +1,6 @@
+//make a simple api call to get the data
+var data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => json);
+//print the data
+console.log(data);
