e69c054cbd44d8c26dc97b511d7e27429e317958
Bm Shadman Sakib Mahee <fareless.ranger@gmail.com>
2023-05-06 00:21:00 +0600

    Update lol.js

--- lol.js
+++ lol.js
@@ -3,6 +3,7 @@ var data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => json);
 //print the data
+//edited simply
 console.log(data);
 function ajaira(){
     alert('Hello World')
